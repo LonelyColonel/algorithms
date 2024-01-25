c928a623e480f25eab1c640d392bcbe509467d28
Lonely_Colonel <lonely_colonel@mail.ru>
2024-01-25 23:55:40 +0300

    new algorithms

---
+++ sortings/comparison_sorts/insertion_sort.py
@@ -0,0 +1,10 @@
+def insertion_sort(nums: list[int]) -> list[int]:
+    for i in range(len(nums)):
+        j = i
+        while (j > 0) and (nums[j - 1] > nums[j]):
+            nums[j], nums[j - 1] = nums[j - 1], nums[j]
+            j -= 1
+    return nums
+
+
+print(insertion_sort([3, 4, 0, 19, 10, 11, 1, 3]))
